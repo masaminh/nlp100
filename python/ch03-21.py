45fa180fa70a9c5f3d89e75704651853af74fcb1
Masami Asakawa <masami.askw@gmail.com>
2017-12-20 23:28:24 +0900

    入力を変更、および関数構成の変更

--- python/ch03-21.py
+++ python/ch03-21.py
@@ -1,17 +1,31 @@
 import argparse
+import gzip
+import json
 import re
+
+
+def get_article(file, name):
+    with gzip.open(file, 'rt') as f:
+        for line in f:
+            dic = json.loads(line)
+
+            if dic["title"] == name:
+                return dic["text"]
+
+
+def get_categorylines(article):
+    regex = re.compile(r'\[\[Category:.*\]\]')
+    return [l for l in article.splitlines() if regex.search(l)]
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('infile', type=argparse.FileType('r'))
+    parser.add_argument('infile')
     args = parser.parse_args()
 
-    regex = re.compile(r'\[\[Category:.*\]\]')
-    for line in args.infile:
-        match = regex.search(line)
-        if match:
-            print(line, end='')
+    article = get_article(args.infile, 'イギリス')
+    categorylines = get_categorylines(article)
+    print('\n'.join(categorylines))
 
 
 if __name__ == '__main__':
