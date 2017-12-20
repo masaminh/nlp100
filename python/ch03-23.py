f29427e54ff78e8090ce5dce73870c5000c24514
Masami Asakawa <masami.askw@gmail.com>
2017-12-21 08:13:24 +0900

    入力の変更、および関数構成の変更

--- python/ch03-23.py
+++ python/ch03-23.py
@@ -1,17 +1,33 @@
 import argparse
+import gzip
+import json
 import re
+
+
+def get_article(file, name):
+    with gzip.open(file, 'rt') as f:
+        for line in f:
+            dic = json.loads(line)
+
+            if dic["title"] == name:
+                return dic["text"]
+
+
+def get_sections(article):
+    regex = re.compile(r'(={2,6})\s*(.+?)\s*\1')
+    return [(m.group(2), len(m.group(1)) - 1) for m
+            in [regex.search(l) for l in article.splitlines()] if m]
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('infile', type=argparse.FileType('r'))
+    parser.add_argument('infile')
     args = parser.parse_args()
 
-    regex = re.compile(r'(={2,6})\s*(?P<name>.+?)\s*\1')
-    for line in args.infile:
-        match = regex.search(line)
-        if match:
-            print(match.group('name') + '\t' + str(len(match.group(1)) - 1))
+    article = get_article(args.infile, 'イギリス')
+    sections = get_sections(article)
+
+    print('\n'.join(['{0[0]}\t{0[1]}'.format(x) for x in sections]))
 
 
 if __name__ == '__main__':
