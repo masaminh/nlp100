69640c839ea905b2e4afad6193f311ef7e354189
Masami Asakawa <masami.askw@gmail.com>
2017-12-20 23:41:23 +0900

    入力の変更、および関数構成の変更

--- python/ch03-22.py
+++ python/ch03-22.py
@@ -1,17 +1,32 @@
 import argparse
+import gzip
+import json
 import re
+
+
+def get_article(file, name):
+    with gzip.open(file, 'rt') as f:
+        for line in f:
+            dic = json.loads(line)
+
+            if dic["title"] == name:
+                return dic["text"]
+
+
+def get_categories(article):
+    regex = re.compile(r'\[\[Category:([^|]+)(\|.*)?\]\]')
+    return [m.group(1) for m
+            in [regex.search(l) for l in article.splitlines()] if m]
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('infile', type=argparse.FileType('r'))
+    parser.add_argument('infile')
     args = parser.parse_args()
 
-    regex = re.compile(r'\[\[Category:(?P<category>[^|]+)(\|.*)?\]\]')
-    for line in args.infile:
-        match = regex.search(line)
-        if match:
-            print(match.group('category'))
+    article = get_article(args.infile, 'イギリス')
+    categories = get_categories(article)
+    print('\n'.join(categories))
 
 
 if __name__ == '__main__':
