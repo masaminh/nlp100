c13863ba0577a54a8087739508d0b4bd78a39d5f
Masami Asakawa <masami.askw@gmail.com>
2017-12-21 22:47:23 +0900

    入力ファイルの変更、および関数構成変更

--- python/ch03-24.py
+++ python/ch03-24.py
@@ -1,17 +1,33 @@
 import argparse
+import gzip
+import json
 import re
+
+
+def get_article(file, name):
+    with gzip.open(file, 'rt') as f:
+        for line in f:
+            dic = json.loads(line)
+
+            if dic["title"] == name:
+                return dic["text"]
+
+
+def get_filereferences(article):
+    regex = re.compile(r'ファイル:(.+?)\|')
+    return [m.group(1) for m
+            in [regex.search(l) for l in article.splitlines()] if m]
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('infile', type=argparse.FileType('r'))
+    parser.add_argument('infile')
     args = parser.parse_args()
 
-    regex = re.compile(r'ファイル:(?P<name>.+?)\|')
-    for line in args.infile:
-        match = regex.search(line)
-        if match:
-            print(match.group('name'))
+    article = get_article(args.infile, 'イギリス')
+    filereferences = get_filereferences(article)
+
+    print('\n'.join(filereferences))
 
 
 if __name__ == '__main__':
