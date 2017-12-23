93fa257a4c2693c92ece8853c29f9be8d4382d16
Masami Asakawa <masami.askw@gmail.com>
2017-12-23 10:20:11 +0900

    入力の変更、および関数構成の変更

--- python/ch03-25.py
+++ python/ch03-25.py
@@ -1,25 +1,44 @@
 import argparse
+import gzip
+import json
 import re
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('infile', type=argparse.FileType('r'))
-    args = parser.parse_args()
+def get_article(file, name):
+    with gzip.open(file, 'rt') as f:
+        for line in f:
+            dic = json.loads(line)
 
+            if dic["title"] == name:
+                return dic["text"]
+
+
+def get_basicinfo(article):
     regex = re.compile(r'^{{基礎情報 国$(.+?)^}}$',
                        flags=(re.MULTILINE | re.DOTALL))
-    alltext = args.infile.read()
-    match = regex.search(alltext)
+    match = regex.search(article)
 
     if match:
         content = match.group(1)
         regex2 = re.compile(r'^\|(.+?) = (.+?)(?:(?=\n\|)|(?=\n$))',
                             flags=(re.MULTILINE | re.DOTALL))
         result = {m.group(1): m.group(2) for m in regex2.finditer(content)}
+    else:
+        result = dict()
 
-        for k, v in result.items():
-            print('{0}\t{1}'.format(k, v))
+    return result
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('infile')
+    args = parser.parse_args()
+
+    article = get_article(args.infile, 'イギリス')
+    basicinfo = get_basicinfo(article)
+
+    for k, v in basicinfo.items():
+        print('{0}\t{1}'.format(k, v))
 
 
 if __name__ == '__main__':
