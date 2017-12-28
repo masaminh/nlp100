9200866462b6f9c1d3c997a7ebf2e143a99e79f1
Masami Asakawa <masami.askw@gmail.com>
2017-12-28 22:04:09 +0900

    入力の変更、および関数構成変更

--- python/ch03-28.py
+++ python/ch03-28.py
@@ -1,5 +1,16 @@
 import argparse
+import gzip
+import json
 import re
+
+
+def get_article(file, name):
+    with gzip.open(file, 'rt') as f:
+        for line in f:
+            dic = json.loads(line)
+
+            if dic["title"] == name:
+                return dic["text"]
 
 
 def remove_markup(s):
@@ -31,15 +42,10 @@ def remove_markup(s):
     return s
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('infile', type=argparse.FileType('r'))
-    args = parser.parse_args()
-
+def get_basicinfo(article):
     regex = re.compile(r'^{{基礎情報 国$(.+?)^}}$',
                        flags=(re.MULTILINE | re.DOTALL))
-    alltext = args.infile.read()
-    match = regex.search(alltext)
+    match = regex.search(article)
 
     if match:
         content = match.group(1)
@@ -47,9 +53,22 @@ def main():
                             flags=(re.MULTILINE | re.DOTALL))
         result = {m.group(1): remove_markup(m.group(2))
                   for m in regex2.finditer(content)}
+    else:
+        result = dict()
 
-        for k, v in result.items():
-            print('{0}\t{1}'.format(k, v))
+    return result
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('infile')
+    args = parser.parse_args()
+
+    article = get_article(args.infile, 'イギリス')
+    basicinfo = get_basicinfo(article)
+
+    for k, v in basicinfo.items():
+        print('{0}\t{1}'.format(k, v))
 
 
 if __name__ == '__main__':
