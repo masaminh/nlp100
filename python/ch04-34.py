5e335249be21dbb5a07e867e7a685169459d2314
Masami Asakawa <masami.askw@gmail.com>
2018-01-01 11:04:52 +0900

    34. 「AのB」

---
+++ python/ch04-34.py
@@ -0,0 +1,48 @@
+import argparse
+from itertools import chain
+
+
+def read_morphemes(lines):
+    morphemes = []
+
+    for line in lines:
+        if line.rstrip() == 'EOS':
+            if len(morphemes) > 0:
+                yield morphemes
+
+            morphemes = []
+        else:
+            t1 = line.split('\t')
+            t2 = t1[1].split(',')
+            m = {'surface': t1[0], 'base': t2[6], 'pos': t2[0], 'pos1': t2[1]}
+            morphemes.append(m)
+
+
+def ngram(s, n):
+    return [s[i:i + n] for i in range(len(s))]
+
+
+def get_words(morphemes):
+    ngrams = chain.from_iterable(ngram(x, 3) for x in morphemes)
+    words = [x[0]['surface'] + x[1]['surface'] + x[2]['surface']
+             for x in ngrams
+             if len(x) == 3 and x[0]['pos'] == '名詞' and x[1]['surface'] == 'の'
+             and x[2]['pos'] == '名詞']
+    return words
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('infile')
+    args = parser.parse_args()
+
+    with open(args.infile, mode='r') as f:
+        morphemes = read_morphemes(f)
+        words = get_words(morphemes)
+
+    for word in words:
+        print(word)
+
+
+if __name__ == '__main__':
+    main()
